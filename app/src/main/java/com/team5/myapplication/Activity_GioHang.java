65418cb7bc4efc4a47d6826d35eba021106befc3
AnnnNguyenn <anntt19411c@st.uel.edu.vn>
2021-11-01 20:54:17 +0700

    them code

---
+++ app/src/main/java/com/team5/myapplication/Activity_GioHang.java
@@ -0,0 +1,14 @@
+package com.team5.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_GioHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gio_hang);
+    }
+}
