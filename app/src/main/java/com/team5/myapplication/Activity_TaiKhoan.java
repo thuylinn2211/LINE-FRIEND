b60f6759002b3d1e7db2074af3476ef739106bf4
AnnnNguyenn <anntt19411c@st.uel.edu.vn>
2021-11-02 21:03:37 +0700

    trang tai khoan 1

---
+++ app/src/main/java/com/team5/myapplication/Activity_TaiKhoan.java
@@ -0,0 +1,14 @@
+package com.team5.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_TaiKhoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tai_khoan);
+    }
+}
