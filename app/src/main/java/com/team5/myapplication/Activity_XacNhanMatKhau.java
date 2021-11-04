7de9e9fa65c5b4e6b170f7093c4d61e02965bb87
AnnnNguyenn <anntt19411c@st.uel.edu.vn>
2021-11-04 09:47:59 +0700

    doi mat khau

---
+++ app/src/main/java/com/team5/myapplication/Activity_XacNhanMatKhau.java
@@ -0,0 +1,14 @@
+package com.team5.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_XacNhanMatKhau extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xac_nhan_mat_khau);
+    }
+}
