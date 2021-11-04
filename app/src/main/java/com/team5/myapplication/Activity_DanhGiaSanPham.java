067bb944985bd3484907d4f9cd0ec4ea4882ecfb
AnnnNguyenn <anntt19411c@st.uel.edu.vn>
2021-11-04 17:58:41 +0700

    danh gia

---
+++ app/src/main/java/com/team5/myapplication/Activity_DanhGiaSanPham.java
@@ -0,0 +1,14 @@
+package com.team5.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_DanhGiaSanPham extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_gia_san_pham);
+    }
+}
