a0181bc68dc0e9d185010809a7b830e570a120b6
Zendo0612 <thanhphuong0612ttl@gmail.com>
2021-11-05 01:26:51 +0700

    pro

--- app/src/main/java/com/team5/myapplication/Activity_LikedProducts.java
+++ app/src/main/java/com/team5/myapplication/Activity_LikedProducts.java
@@ -3,12 +3,43 @@ package com.team5.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import com.team5.myapplication.model.Products;
+
+import java.util.ArrayList;
 
 public class Activity_LikedProducts extends AppCompatActivity {
+
+    GridView gvProducts;
+
+    ArrayAdapter<String> adapter;
+    ArrayList<Products> products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liked_products);
+
+        linkViews();
+        initAdapter();
+        getData();
     }
+
+    private void linkViews() {
+        gvProducts = findViewById(R.id.gvProducts);
+    }
+
+    private void initAdapter() {
+        adapter = new ArrayAdapter<String>(Activity_LikedProducts.this, R.layout.item_products);
+        gvProducts.setAdapter(adapter);
+    }
+
+    private void getData() {
+        products  = new ArrayList<Products>();
+        products.add(new Products(R.drawable.butbihinhtraitim, "Bút bi hình trái tim"));
+        products.add(new Products(R.drawable.gaubonghinhtraitim, "Gấu bông hình trái tim"));
+    }
+
 }
---
+++ app/src/main/java/com/team5/myapplication/model/Products.java
@@ -0,0 +1,18 @@
+package com.team5.myapplication.model;
+
+public class Products {
+    private  String productName;
+
+    public Products(int butbihinhtraitim, String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
+
