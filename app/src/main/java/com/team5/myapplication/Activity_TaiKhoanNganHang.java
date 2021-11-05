79e99d0b5fd3817db26cd0328a8113c78c58534b
Zendo0612 <thanhphuong0612ttl@gmail.com>
2021-11-05 19:49:07 +0700

    payment

---
+++ app/src/main/java/com/team5/myapplication/Activity_TaiKhoanNganHang.java
@@ -0,0 +1,14 @@
+package com.team5.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_TaiKhoanNganHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tai_khoan_ngan_hang);
+    }
+}
