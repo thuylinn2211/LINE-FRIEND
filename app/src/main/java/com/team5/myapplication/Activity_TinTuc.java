bda429b814925168b1fc8b0ca669fea27c617a30
thuylinn2211 <linhptt19411c@st.uel.edu.vn>
2021-12-16 22:44:20 +0700

    bổ sung 3 fragment tin tức

--- app/src/main/java/com/team5/myapplication/Activity_TinTuc.java
+++ app/src/main/java/com/team5/myapplication/Activity_TinTuc.java
@@ -1,14 +1,40 @@
 package com.team5.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class Activity_TinTuc extends AppCompatActivity {
+public class Activity_TinTuc extends AppCompatActivity implements View.OnClickListener{
+
+    LinearLayout lvTinTuc1, lvTinTuc2, lvTinTuc3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tin_tuc);
+
+        linkViews();
+
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.lvTinTuc1, new frag_tintuc1()).commit();
     }
+
+    private void linkViews() {
+        lvTinTuc1 = findViewById(R.id.lvTinTuc1);
+        lvTinTuc2 = findViewById(R.id.lvTinTuc2);
+        lvTinTuc3 = findViewById(R.id.lvTinTuc3);
+    }
+
+    public void onClick(View view) {
+        if(view.getId() == R.id.lvTinTuc1){
+            getSupportFragmentManager().beginTransaction().replace(R.id.lvTinTuc1, new frag_tintuc1()).commit();
+            lvTinTuc1.setVisibility(View.GONE);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/team5/myapplication/frag_tintuc1.java
@@ -0,0 +1,14 @@
+package com.team5.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class frag_tintuc1 extends Fragment {
+
+}
