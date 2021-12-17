7335f349c4b11cdf26e7652c81bfdb89c40efc8b
thuylinn2211 <linhptt19411c@st.uel.edu.vn>
2021-12-17 11:11:35 +0700

    replace search -> timkiem, and complete spinner tỉnh thành của activity frag thanh toán chỉnh sửa đơn hàng

--- app/src/main/java/com/team5/myapplication/Activity_TimKiem.java
+++ app/src/main/java/com/team5/myapplication/Activity_TimKiem.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity_SearchProduct extends AppCompatActivity {
+public class Activity_TimKiem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_product);
+        setContentView(R.layout.activity_tim_kiem);
     }
 }
--- app/src/main/java/com/team5/myapplication/Activity_frag_thanhtoan_chinhsuadonhang.java
+++ app/src/main/java/com/team5/myapplication/Activity_frag_thanhtoan_chinhsuadonhang.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 public class Activity_frag_thanhtoan_chinhsuadonhang extends AppCompatActivity {
 
-    private String selectedTinh, selectedHuyen, selectedXa;
+    String selectedTinh, selectedHuyen, selectedXa;
     Spinner tinhSpinner, huyenSpinner, xaSpinner;
     ArrayAdapter<CharSequence> tinhAdapter, huyenAdapter, xaAdapter;
 
@@ -26,6 +26,7 @@ public class Activity_frag_thanhtoan_chinhsuadonhang extends AppCompatActivity {
 
     private void linkViews() {
         tinhSpinner = findViewById(R.id.spTinh);
+
     }
 
     private void loadData() {
@@ -52,17 +53,63 @@ public class Activity_frag_thanhtoan_chinhsuadonhang extends AppCompatActivity {
                         case "Hà Nam": huyenAdapter = ArrayAdapter.createFromResource(parent.getContext(),
                                 R.array.array_hanam_huyen, R.layout.fragment_spinner_layout);
                         break;
+                        case "An Giang": huyenAdapter = ArrayAdapter.createFromResource(parent.getContext(),
+                                R.array.array_angiang_huyen, R.layout.fragment_spinner_layout);
+                        break;
                         default: break;
                     }
+
+                    //selected huyen --> xa
                     huyenAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     huyenSpinner.setAdapter(huyenAdapter);
+                    huyenSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                            //tìm xã
+                            xaSpinner = findViewById(R.id.spXa);
+                            selectedHuyen = huyenSpinner.getSelectedItem().toString();
+
+                            int parentID = parent.getId();
+                            if (parentID == R.id.spHuyen) {
+                                switch (selectedHuyen) {
+                                    case "Quận/ Huyện (*)":
+                                        xaAdapter = ArrayAdapter.createFromResource(parent.getContext(),
+                                                R.array.arrray_default_xa, R.layout.fragment_spinner_layout);
+                                        break;
+                                    case "Thành phố Long Xuyên":
+                                        xaAdapter = ArrayAdapter.createFromResource(parent.getContext(),
+                                                R.array.array_angiang_thanhpholongxuyen_xa, R.layout.fragment_spinner_layout);
+                                        break;
+                                    default: break;
+                                }
+
+                                xaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                xaSpinner.setAdapter(xaAdapter);
+
+                                xaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                    @Override
+                                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                        selectedXa = xaSpinner.getSelectedItem().toString();
+                                    }
+
+                                    @Override
+                                    public void onNothingSelected(AdapterView<?> parent) {
+                                    }
+                                });
+                            }
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
                 }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
     }
 }
