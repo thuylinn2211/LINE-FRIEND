90187d1bbc2a00823f2e80a6c04089e915781a76
thuylinn2211 <linhptt19411c@st.uel.edu.vn>
2021-12-17 16:34:29 +0700

    ic camera

--- app/src/main/java/com/team5/myapplication/Activity_TimKiem.java
+++ app/src/main/java/com/team5/myapplication/Activity_TimKiem.java
@@ -3,12 +3,21 @@ package com.team5.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class Activity_TimKiem extends AppCompatActivity {
+
+    public static final String[] SANPHAM = new String[]{"BT21", "Missha", "Móc khóa Thỏ Cony", "Gấu bông"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tim_kiem);
+
+        AutoCompleteTextView edtTimKiem = findViewById(R.id.edtTimKiem);
+        ArrayAdapter<String> spAdapter = new ArrayAdapter<String>(Activity_TimKiem.this, android.R.layout.simple_list_item_1, SANPHAM);
+        edtTimKiem.setAdapter(spAdapter);
     }
 }
