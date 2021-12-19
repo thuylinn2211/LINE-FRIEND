edf05511c2a6b95f8e7e65e9df9e37c2cad6efa7
AnnnNguyenn <anntt19411c@st.uel.edu.vn>
2021-12-19 16:02:58 +0700

    Chinh Giao dien GioHang

--- app/src/main/java/com/team5/myapplication/Activity_GioHang.java
+++ app/src/main/java/com/team5/myapplication/Activity_GioHang.java
@@ -3,12 +3,48 @@ package com.team5.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.team5.model.SanPham;
+
+import java.text.DecimalFormat;
 
 public class Activity_GioHang extends AppCompatActivity {
+    ImageView imvPhoto;
+    TextView txtTenSanPham, txtGia, txtGiamGia;
+
+    Button btnMuaHang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gio_hang);
+
+        linkViews();
+        addEvents();
+        getData();
+    }
+
+    private void linkViews() {
+        imvPhoto = findViewById(R.id.imvPhoto);
+        txtTenSanPham = findViewById(R.id.txtTenSanPham);
+        txtGia = findViewById(R.id.txtGia);
+        txtGiamGia = findViewById(R.id.txtGiamGia);
+        btnMuaHang = findViewById(R.id.btnMuaHang);
+    }
+
+    private void addEvents() {
+        btnMuaHang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void getData() {
     }
 }
